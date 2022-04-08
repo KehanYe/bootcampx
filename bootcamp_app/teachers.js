9c265d92c8821fdb8f7febf345986c380f17f0bb
Kehan Fu <kehanfu@gmail.com>
2022-04-08 22:18:43 +0000

    added parameterized query

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,19 +7,30 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-  SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
-  FROM teachers
-  JOIN assistance_requests ON teachers.id = teacher_id
-  JOIN students ON students.id = student_id
-  JOIN cohorts ON cohorts.id = cohort_id
-  WHERE cohorts.name LIKE '%${process.argv[2] || 'JUL02' }%'
-  ORDER BY teacher
-  LIMIT ${process.argv[3] || 10};
-`)
+//potentially malicious input from user. Stored into values array
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
+
+
+//unmalicious text
+// $ references the ability to reference the pull object built-in 
+const queryString = `SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE $1
+ORDER BY teachers.name
+LIMIT $2;`
+
+//pool.query is a function that accepts an SQL query as a string.
+//the rows property will have an array with the expected results
+pool.query(queryString, values)
 .then(res => {
+  // console.log(res.rows)
   res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
+    console.log(`${row.teacher}: ${row.cohort}`);
   })
 })
 .catch(err => console.error('query error', err.stack));
